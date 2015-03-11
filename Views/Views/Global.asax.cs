4f1bc7909039759614404f7c569755c4693f6479
philipmward@hotmail.com <philipmward@hotmail.com>
2015-03-11 14:30:54 -0700

    commit

---
+++ Views/Views/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Views.Infrastructure;
+
+namespace Views
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Add((new DebugDataViewEngine()));
+        }
+    }
+}
